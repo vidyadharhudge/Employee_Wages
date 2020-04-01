51bbeccbaec662126fc6499dc75064dd340b5512
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-01 12:21:52 +0530

    Added:-Employee Prsenty

--- src/main/java/com/Emp_Wage/Employee_Wage.java
+++ src/main/java/com/Emp_Wage/Employee_Wage.java
@@ -1,10 +1,18 @@
 package com.Emp_Wage;
 
+import java.util.Random;
+
 public class Employee_Wage
 {
     public static void main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Program");
+        Random random=new Random();
+        int number=random.nextInt(2);
+        if(number==0)
+            System.out.println("Employee Is Prsent");
+        else
+            System.out.println("Employee Is Absent");
 
     }
 }
