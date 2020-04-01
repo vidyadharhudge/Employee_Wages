a0781433affcb16b67abd97d706debbbdf3efa82
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-01 13:47:17 +0530

    Added:-Calculate Part Time Employee Wages

--- src/main/java/com/Emp_Wage/Employee_Wage.java
+++ src/main/java/com/Emp_Wage/Employee_Wage.java
@@ -23,22 +23,32 @@ public class Employee_Wage
     {
         public static void main(String[] args)
         {
-            int Employee_Wage_PER_HOUR = 20;
+            int EMPLOYEE_WEG_PER_HOUR = 20;
             int FULL_DAY_HOUR = 8;
+            int PARTIME_HOUR = 4;
             int ABSENT = 0;
             int EMPLOYEE_PRSENT_OR_ABSENT;
+            int PARTIME_OR_FULLTIME;
             int EMPLOYEE_WEGES;
             Employee_Wage emp = new Employee_Wage();
             EMPLOYEE_PRSENT_OR_ABSENT = emp.Check_Attendence();
             if (EMPLOYEE_PRSENT_OR_ABSENT == 0)
             {
-                EMPLOYEE_WEGES = emp.Calculate_Daily_Emoployee_Wages(Employee_Wage_PER_HOUR, FULL_DAY_HOUR);
+                PARTIME_OR_FULLTIME = emp.Check_Attendence();
+                if (PARTIME_OR_FULLTIME == 0)
+                {
+                    EMPLOYEE_WEGES = emp.Calculate_Daily_Emoployee_Wages(EMPLOYEE_WEG_PER_HOUR, FULL_DAY_HOUR);
+                }
+                else
+                {
+                    EMPLOYEE_WEGES = emp.Calculate_Daily_Emoployee_Wages(EMPLOYEE_WEG_PER_HOUR, PARTIME_HOUR);
+                }
             }
             else
             {
                 EMPLOYEE_WEGES = ABSENT;
             }
-            System.out.println("Daily Employee Wage is : "+EMPLOYEE_WEGES);
+                System.out.println("Daily Employee Wage is : "+EMPLOYEE_WEGES);
 
         }
     }
