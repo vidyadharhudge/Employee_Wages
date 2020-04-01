2ed22d3b057f2dd89510762a170ba57ce93ba538
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-01 11:10:05 +0530

    Added:-Welcome Message

---
+++ src/main/java/com/Emp_Wage/Employee_Wage.java
@@ -0,0 +1,10 @@
+package com.Emp_Wage;
+
+public class Employee_Wage
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Employee Wage Program");
+
+    }
+}
